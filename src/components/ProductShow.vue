34380334ccb3adb4de780d1e24089c4ceb5e23bd
SawSanTun <sst@itgateway.org>
2024-02-12 12:29:33 +0630

    Laptop Product add

---
+++ src/components/ProductShow.vue
@@ -0,0 +1,12 @@
+<template>
+  <Laptop></Laptop>
+</template>
+
+<script>
+import Laptop from "./Product/Laptop";
+export default {
+  components: { Laptop },
+};
+</script>
+
+<style></style>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -3,15 +3,18 @@
   <Slider></Slider>
   <PopularSilder></PopularSilder>
   <BrowseBrand></BrowseBrand>
+  <ProductShow></ProductShow>
 </template>
 
 <script>
+import ProductShow from "../components/ProductShow";
 import BrowseBrand from "../components/BrowseBrand";
 import PopularSilder from "../components/PopularSilder";
 import Slider from "../components/Slider";
 import Header from "../components/Header";
 export default {
   components: {
+    ProductShow,
     BrowseBrand,
     PopularSilder,
     Slider,
