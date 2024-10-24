daaab9b49f3ca50483752c9abce7ad2a1ebdc622
SawSanTun-itGateway <sst@itgatewaymm.com>
2024-10-24 20:26:40 +0700

    loading add

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,23 @@
   <v-app>
     <Navbar @menuHoverStatus="menuHoverEffect"></Navbar>
     <v-main :class="{ blurPage: isHover }">
+      <Loading v-if="loading" />
       <router-view />
     </v-main>
     <Footer></Footer>
   </v-app>
+  <Loading v-if="loading" />
 </template>
 
 <script>
+import Loading from "./components/Loading";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-import { ref } from "vue";
+import { ref, watch, onMounted } from "vue";
+import { useRoute } from "vue-router";
 export default {
   components: {
+    Loading,
     Footer,
     Navbar,
   },
@@ -21,10 +26,33 @@ export default {
   setup() {
     const isHover = ref(false);
     const menuHoverEffect = (data) => (isHover.value = data);
+    const route = useRoute();
+
+    const loading = ref(true);
+
+    const loadContent = () => {
+      loading.value = true;
+
+      setTimeout(() => {
+        loading.value = false;
+      }, 3000);
+    };
+
+    watch(route, () => loadContent());
+
+    onMounted(() => {
+      // window.addEventListener("load", () => {
+      //   loading.value = false;
+      // });
+      window.scroll(0, 0);
+      loadContent();
+    });
 
     return {
       menuHoverEffect,
       isHover,
+      loadContent,
+      loading,
     };
   },
 };
